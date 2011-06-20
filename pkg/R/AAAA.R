b185c12053044d38139384f12da5cd5e9371ff6a
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-20 09:59:34 +0000

    Added layer() method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/AAAA.R
+++ pkg/R/AAAA.R
@@ -1,3 +1,8 @@
+if (!isGeneric("layer"))
+  setGeneric("layer", function(obj, ...)
+    standardGeneric("layer")
+  )
+
 if (!isGeneric("kml"))
   setGeneric("kml", function(obj, ...)
     standardGeneric("kml")
