44c98aefc2cb49b149dc9bfd295263cbbeac76b1
roudierp (Dr. ) <pierre.roudier@gmail.com>
2011-06-22 14:38:15 +0000

    code tidying and first integration of aesthetics primitives
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/layer.SpatialPoints.R
+++ pkg/R/layer.SpatialPoints.R
@@ -32,7 +32,7 @@ layer.SpatialPoints <- function(
   file = "points.kml",
   size = as.character(NA),
   colour = as.character(NA),
-  elevation = NA, 
+  altitude = NA, 
 
   # color scheme
   col.region = rainbow(64),
@@ -46,7 +46,7 @@ layer.SpatialPoints <- function(
   icon = "http://maps.google.com/mapfiles/kml/shapes/donut.png", 
   extrude = TRUE, 
   z.scale = 1, 
-  elevation.default = 10,
+  altitude.default = 10,
   LabelScale = 0.7, 
   kmz = FALSE,
   kml.url = "http://www.opengis.net/kml/2.2",
@@ -73,101 +73,99 @@ layer.SpatialPoints <- function(
   # be used using the following form:
   #   size.values <- aes[["size"]]
   #   colour.values <- aes[["colour"]]
+  aes <- kml_aes(obj, ...)
 
-  # Computing the size values vector
-  if (!is.na(size)) {
-    if (is.numeric(obj[[size]])) {
-      maxvar.size <- max(obj[[size]], na.rm = TRUE)
-      size.values <- (obj[[size]] / maxvar.size) * plt.size.max + plt.size.min
-    }
-  }
+  # read the relevant aesthetics
+  size.values <- aes[["size"]]
+  colours.values <- aes[["colour"]]
+  altitude <- aes[["altitude"]]
+  altitudeMode <- aes[["altitudeMode"]]
 
-  # Computing colours values vector
-  if (!is.na(colour)) {
-    if (is.numeric(obj[[colour]])) {
-      minvar.colour <- min(obj[[colour]], na.rm = TRUE)
-      maxvar.colour <- max(obj[[colour]], na.rm = TRUE)
-      brks <- seq(min(obj[[colour]], na.rm = TRUE), max(obj[[colour]], na.rm = TRUE), length.out = length(col.region))
-      grps <- cut(obj[[colour]], breaks = brks, include.lowest = TRUE)
-      colour.values <- col.region[grps]
-    }
-  }
-
-  # Computing the elevation values vector
-  if (is.character(elevation)) {
-    # The character describes the name of a column
-    elevation.mode <- "absolute"
-    elevation <- obj[[elevation]]
-  }
-  else if (is.numeric(elevation)) {
-    # If it is numeric this is a single elevation for all points
-    elevation.mode <- "relativeToGround"
-    elevation <- rep(elevation, length.out = nrow(obj))
-  }
-  else if (is.na(elevation)) {
-    elevation.mode <- "clampToGround"
-    elevation <- rep(elevation.default, length.out = nrow(obj))
-  }
-  else   
-    stop("Bad elevation value")
+#   # Computing the size values vector
+#   if (!is.na(size)) {
+#     if (is.numeric(obj[[size]])) {
+#       maxvar.size <- max(obj[[size]], na.rm = TRUE)
+#       size.values <- (obj[[size]] / maxvar.size) * plt.size.max + plt.size.min
+#     }
+#   }
+# 
+#   # Computing colours values vector
+#   if (!is.na(colour)) {
+#     if (is.numeric(obj[[colour]])) {
+#       minvar.colour <- min(obj[[colour]], na.rm = TRUE)
+#       maxvar.colour <- max(obj[[colour]], na.rm = TRUE)
+#       brks <- seq(min(obj[[colour]], na.rm = TRUE), max(obj[[colour]], na.rm = TRUE), length.out = length(col.region))
+#       grps <- cut(obj[[colour]], breaks = brks, include.lowest = TRUE)
+#       colour.values <- col.region[grps]
+#     }
+#   }
+# 
+#   # Computing the elevation values vector
+#   if (is.character(elevation)) {
+#     # The character describes the name of a column
+#     elevation.mode <- "absolute"
+#     elevation <- obj[[elevation]]
+#   }
+#   else if (is.numeric(elevation)) {
+#     # If it is numeric this is a single elevation for all points
+#     elevation.mode <- "relativeToGround"
+#     elevation <- rep(elevation, length.out = nrow(obj))
+#   }
+#   else if (is.na(elevation)) {
+#     elevation.mode <- "clampToGround"
+#     elevation <- rep(elevation.default, length.out = nrow(obj))
+#   }
+#   else   
+#     stop("Bad elevation value")
 
   # Folder and name of the points folder
-  cat("<Folder>\n", file = kml, append = TRUE)
-  cat("<name>", as.character(substitute(obj, env = parent.frame())),"</name>\n", sep = "", file = kml, append = TRUE)
+  cat("<Folder>\n", file = file, append = TRUE)
+  cat("<name>", as.character(substitute(obj, env = parent.frame())),"</name>\n", sep = "", file = file, append = TRUE)
 
   # Writing points styles
   # =====================
-  for (i in 1:nrow(obj)) {
-    cat('\t<Style id="','pnt', i,'">\n',sep = "", file = kml, append = TRUE)
-    cat("\t\t<LabelStyle>\n", file = kml, append = TRUE)     
-    cat('\t\t\t<scale>', LabelScale, '</scale>\n', sep = "", file = kml, append = TRUE)
-    cat("\t\t</LabelStyle>\n", file = kml, append = TRUE)
-    cat("\t\t<IconStyle>\n", file = kml, append = TRUE)
+  for (i_pt in 1:nrow(obj)) {
+    cat('\t<Style id="','pnt', i_pt,'">\n',sep = "", file = file, append = TRUE)
 
-    # If there is a particular variable to be represented by colour scale
-    if (!is.na(colour) & is.numeric(obj[[colour]])) {
-      cat('\t\t\t<color>', tolower(col2hcl(colour.values[i])), '</color>\n', sep = "", file = kml, append = TRUE)
-    }
-    else {
-      cat('\t\t\t<color>', tolower(col2hcl(col.default)), '</color>\n', sep = "", file = kml, append = TRUE)
-    }
+    # Label
+    cat("\t\t<LabelStyle>\n", file = file, append = TRUE)     
+    cat('\t\t\t<scale>', LabelScale, '</scale>\n', sep = "", file = file, append = TRUE)
+    cat("\t\t</LabelStyle>\n", file = file, append = TRUE)
 
-    # If there is a particular variable to be represented by size scale
-    if (!is.na(size) & is.numeric(obj[[size]])) {
-      cat("\t\t\t<scale>", size.values[i], "</scale>\n", sep = "", file = kml, append = TRUE)
-    }
-    else {
-      cat("\t\t\t<scale>", plt.size.default, "</scale>\n", sep = "", file = kml, append = TRUE)
-    }
+    # Icon
+    cat("\t\t<IconStyle>\n", file = file, append = TRUE)
 
-    cat("\t\t\t<Icon>\n", file = kml, append = TRUE)
-    cat('\t\t\t\t<href>', icon, '</href>\n', sep = "", file = kml, append = TRUE)
-    cat("\t\t\t</Icon>\n", file = kml, append = TRUE)
-    cat("\t\t</IconStyle>\n", file = kml, append = TRUE)
-    cat("\t</Style>\n", file = kml, append = TRUE)
+    # Aesthetics
+    cat('\t\t\t<color>', colour.values[i_pt], '</color>\n', sep = "", file = file, append = TRUE)
+    cat("\t\t\t<scale>", size.values[i_pt], "</scale>\n", sep = "", file = file, append = TRUE)
+    cat("\t\t\t<Icon>\n", file = file, append = TRUE)
+    cat('\t\t\t\t<href>', icon, '</href>\n', sep = "", file = file, append = TRUE)
+    cat("\t\t\t</Icon>\n", file = file, append = TRUE)
+    cat("\t\t</IconStyle>\n", file = file, append = TRUE)
+    cat("\t</Style>\n", file = file, append = TRUE)
   }
 
   # Writing points coordinates
   # ==========================
-  for (i in 1:nrow(obj)) {
-    cat("\t<Placemark>\n", file = kml, append = TRUE)
-    cat("\t\t<name>", points.names[i],"</name>\n", sep = "", file = kml, append = TRUE)
-    cat("\t\t<styleUrl>#pnt",i,"</styleUrl>\n", sep = "", file = kml, append = TRUE)
-    cat("\t\t<Point>\n", file = kml, append = TRUE)
+  for (i_pt in 1:nrow(obj)) {
+    cat("\t<Placemark>\n", file = file, append = TRUE)
+    cat("\t\t<name>", points.names[i_pt],"</name>\n", sep = "", file = file, append = TRUE)
+    cat("\t\t<styleUrl>#pnt", i_pt,"</styleUrl>\n", sep = "", file = file, append = TRUE)
+    cat("\t\t<Point>\n", file = file, append = TRUE)
 
-    # If there's elevation information to be represented
-    if (sd(elevation, na.rm = TRUE) > 0) {
-      cat('\t\t\t<extrude>', as.numeric(extrude), '</extrude>\n', sep = "", file = kml, append = TRUE)
-      cat('\t\t\t<altitudeMode>', elevation.mode, '</altitudeMode>\n', sep = "", file = kml, append = TRUE)
+    # If there's altitude information to be represented
+    if (sd(altitude, na.rm = TRUE) > 0) {
+      cat('\t\t\t<extrude>', as.numeric(extrude), '</extrude>\n', sep = "", file = file, append = TRUE)
+      cat('\t\t\t<altitudeMode>', altitudeMode, '</altitudeMode>\n', sep = "", file = file, append = TRUE)
     }
 
-    cat("\t\t\t<coordinates>", coordinates(obj)[i, 1], ",", coordinates(obj)[i, 2], ",", elevation[i]*z.scale,"</coordinates>\n", sep = "", file = kml, append = TRUE)
-    cat("\t\t</Point>\n", file = kml, append = TRUE)
-    cat("\t</Placemark>\n", file = kml, append = TRUE)
+    cat("\t\t\t<coordinates>", coordinates(obj)[i_pt, 1], ",", coordinates(obj)[i_pt, 2], ",", altitude[i_pt]*z.scale,"</coordinates>\n", sep = "", file = file, append = TRUE)
+    cat("\t\t</Point>\n", file = file, append = TRUE)
+    cat("\t</Placemark>\n", file = file, append = TRUE)
 
   }
 
   # Closing the folder
-  cat("</Folder>\n", file = kml, append = TRUE)
+  cat("</Folder>\n", file = file, append = TRUE)
   
 }
