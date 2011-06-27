82a65bc5d3038603818ccad3c7556c280850e933
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-27 05:13:51 +0000

    Various fixes and adds to have the package working for SpatialPoints
    - introduced first scaling function for colour (only continuous for the
      moment)
    - Worked out the various functions needed in this file
    - Now parsing also `...` if it has been passed to the kml_aes() function
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/aesthetics.R
+++ pkg/R/aesthetics.R
@@ -14,30 +14,33 @@
 # Parsing a call
 #
 .parse_call_for_aes <- function(call){
-  parent_call <- structure(as.list(call), class="uneval")
-  called_options <- names(parent_call)
+  called_options <- names(call)
   ind_aes <- charmatch(called_options, names(.all_kml_aesthetics))
 
   names(.all_kml_aesthetics)[ind_aes[!is.na(ind_aes)]]
 }
 
 # Applying aesthetics
-# 
+#
 kml_aes <- function(obj, ...){
-  
+
   # Getting parent call
   parent_call <- sys.calls()[[sys.nframe() - 1]]
+  parent_call <- structure(as.list(parent_call), class="uneval")
+  parent_call <- c(parent_call, list(...))
   # Parse the current call
   called_aes <- .parse_call_for_aes(parent_call)
+
+  aes <- list()
 
   # Colour
   if ("colour" %in% called_aes) {
     aes[['colour']] <- kml_colour(obj, colour, ...)
     # Whitening
-    if ("whitening" %in% called_aes) {
-      aes[['colour']] <- kml_whitening(obj, whitening, aes[['colour']], ...)
-    }
-  } 
+#     if ("whitening" %in% called_aes) {
+#       aes[['colour']] <- kml_whitening(obj, whitening, aes[['colour']], ...)
+#     }
+  }
   else {
     aes[['colour']] <- rep(.all_kml_aesthetics[["colour"]], length.out = nrow(coordinates(obj)))
   }
@@ -47,13 +50,13 @@ kml_aes <- function(obj, ...){
     aes[["shape"]] <- kml_shape(obj, shape, ...)
   }
   else {
-    aes[["shape"]] <- .all_kml_aesthetics[["shape"]]
+    aes[["shape"]] <- rep(.all_kml_aesthetics[["shape"]], length.out = nrow(coordinates(obj)))
   }
 
   # Size
   if ("size" %in% called_aes) {
     aes[['size']] <- kml_size(obj, size, ...)
-  } 
+  }
   else {
     aes[['size']] <- rep(.all_kml_aesthetics[["size"]], length.out = nrow(coordinates(obj)))
   }
@@ -61,7 +64,7 @@ kml_aes <- function(obj, ...){
   # Width
   if ("width" %in% called_aes) {
     aes[['width']] <- kml_width(obj, width, ...)
-  } 
+  }
   else {
     aes[['width']] <- rep(.all_kml_aesthetics[["width"]], length.out = nrow(coordinates(obj)))
   }
@@ -69,7 +72,7 @@ kml_aes <- function(obj, ...){
   # Alpha
   if ("alpha" %in% called_aes) {
     aes[['alpha']] <- kml_alpha(obj, alpha, ...)
-  } 
+  }
   else {
     aes[['alpha']] <- rep(.all_kml_aesthetics[["alpha"]], length.out = nrow(coordinates(obj)))
   }
@@ -77,25 +80,55 @@ kml_aes <- function(obj, ...){
   # Altitude
   if ("altitude" %in% called_aes) {
     aes[['altitude']] <- kml_altitude(obj, altitude, ...)
-  } 
+  }
   else {
     aes[['altitude']] <- rep(.all_kml_aesthetics[["altitude"]], length.out = nrow(coordinates(obj)))
   }
 
   # AltitudeMode
   aes[["altitudeMode"]] <- kml_altitude_mode(aes[['altitude']])
-  
+
   aes
 }
 
 # Colour (points, polygons, lines, raster)
-kml_colour <- function(obj, colour, col.region = rainbow(64), col.default = "black"){
-  
+kml_colour <- function(obj, colour, ..., col.region = rainbow(64)){
+
+  x <- obj[[colour]]
+
+  # If the scale is continuous
+  if (is.numeric(x)) {
+    limits <- range(x, na.rm = TRUE, finite = TRUE)
+    brks <- seq(limits[1], limits[2], length.out = length(col.region))
+    grps <- cut(x, breaks = brks, include.lowest = TRUE)
+    cols <- hex2kml(col.region[grps])
+  }
+  # If discrete scale
+  else {
+
+  }
+
+  cols
+}
+
+# Shape (points)
+kml_shape <- function(obj, shape, ...){
+
 }
 
 # Opacity (points, polygons, lines, raster)
-kml_alpha <- function(obj, alpha.default = 1){
+kml_alpha <- function(obj, alpha, colours, ...){
 
+  x <- obj[[alpha]]
+
+  if (is.numeric(x)) {
+    limits <- range(x, na.rm = TRUE, finite = TRUE)
+    brks <- seq(limits[1], limits[2], length.out = length(colours))
+    grps <- cut(x, breaks = brks, include.lowest = TRUE)
+#     alphas <-
+  }
+
+  cols
 }
 
 # Whitening of a given coloured layer (update of the colour vector)
@@ -114,7 +147,7 @@ kml_size <- function(obj, size, size.min = 0.25, size.max = 4, size.default = 2)
     }
     # Otherwise: factor, character, logical, ...
     else {
-      if (!is.factor(obj[[size]])) 
+      if (!is.factor(obj[[size]]))
         obj[[size]] <- factor(obj[[size]])
 
       # compute number of levels
