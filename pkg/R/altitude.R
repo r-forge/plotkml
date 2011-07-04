be6b774d2d64a38079e1e65a2a74e018825b8944
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-04 05:48:28 +0000

    separating aesthetic computation of altitude and altitudeMode
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@74 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/altitude.R
+++ pkg/R/altitude.R
@@ -1,27 +1,24 @@
 # Altitude (points, polygons, lines, raster)
 #
 # Returns an altitude vector and a altitudeMode string
-kml_altitude <- function(obj, altitude = NA, altitude.default = 10){
+kml_altitude <- function(obj, altitude = NA) {
 
   # Testing what has been given by the user
   if (is.character(altitude)) {
     # The character describes the name of a column
-    altitude_mode <- "absolute"
     altitude <- obj[[altitude]]
   }
   else if (is.numeric(altitude)) {
     # If it is numeric this is a single altitude for all points
-    altitude_mode <- "relativeToGround"
     altitude <- rep(altitude, length.out = nrow(obj))
   }
   else if (is.na(altitude)) {
-    altitude_mode <- "clampToGround"
-    altitude <- rep(altitude.default, length.out = nrow(obj))
+    altitude <- rep(NA, length.out = nrow(obj))
   }
   else
     stop("Bad altitude value")
 
-  list(altitude = altitude, altitude.mode = altitude_mode)
+  altitude
 }
 
 # Guesses the appropriate altitudeMode tag
