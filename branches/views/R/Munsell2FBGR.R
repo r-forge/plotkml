c954fc4a3518b8f56a26db2bba9720195742f493
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-08 15:49:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/views/R/Munsell2FBGR.R
+++ branches/views/R/Munsell2FBGR.R
@@ -1,21 +1,30 @@
-Munsell2FBGR <- function(x, Munsell, transp) {
-# Munsell = munsell color codes column;
-# merge munsell color codes in format "7.5R_2_4" and RGB values:
-if(missing(transp)) { transp <- "dd" }
-for(i in 1:length(x[,Munsell])){
-x[i, "Hue"] <- strsplit(as.character(x[i, Munsell]), "_")[[1]][1]
-x[i, "Chroma"] <- as.numeric(strsplit(as.character(x[i, Munsell]), "_")[[1]][2]) 
-x[i,"Value"] <- as.numeric(strsplit(as.character(x[i, Munsell]), "_")[[1]][3])
-}
-mcolor.RGB <- munsell2rgb(the_hue=x$Hue, the_chroma=x$Chroma, the_value=x$Value, return_triplets=TRUE)
-mcolor.RGB$Rc <- round(mcolor.RGB$r*100, 0)
-mcolor.RGB$Gc <- round(mcolor.RGB$g*100, 0)
-mcolor.RGB$Bc <- round(mcolor.RGB$b*100, 0)
-# reformat colors suited for Google Earth:
-Bc <- ifelse(mcolor.RGB$Bc<10, paste("0", mcolor.RGB$Bc, sep=""), ifelse(mcolor.RGB$Bc==100, "ff", mcolor.RGB$Bc))
-Gc <- ifelse(mcolor.RGB$Gc<10, paste("0", mcolor.RGB$Gc, sep=""), ifelse(mcolor.RGB$Gc==100, "ff", mcolor.RGB$Gc))
-Rc <- ifelse(mcolor.RGB$Rc<10, paste("0", mcolor.RGB$Rc, sep=""), ifelse(mcolor.RGB$Rc==100, "ff", mcolor.RGB$Rc))
-FBGR <- paste(transp, Bc, Gc, Rc, sep="")
-return(FBGR)
+# R function for the plotKML package
+# Author: Tomislav Hengl
+# contact: tom.hengl@wur.nl
+# Date : July 2011
+# Version 0.1
+# Licence GPL v3
+
+Munsell2FBGR <- function(
+  x, 
+  Munsell, 
+  transp = "dd"
+  ){
+  require(aqp)
+  for(i in 1:length(x[,Munsell])){
+  x[i,"Hue"] <- strsplit(as.character(x[i, Munsell]), "_")[[1]][1]
+  x[i,"Chroma"] <- as.numeric(strsplit(as.character(x[i, Munsell]), "_")[[1]][2]) 
+  x[i,"Value"] <- as.numeric(strsplit(as.character(x[i, Munsell]), "_")[[1]][3])
+  }
+  mcolor.RGB <- munsell2rgb(the_hue=x$Hue, the_chroma=x$Chroma, the_value=x$Value, return_triplets=TRUE)
+  mcolor.RGB$Rc <- round(mcolor.RGB$r*100, 0)
+  mcolor.RGB$Gc <- round(mcolor.RGB$g*100, 0)
+  mcolor.RGB$Bc <- round(mcolor.RGB$b*100, 0)
+  # reformat colors suited for Google Earth:
+  Bc <- ifelse(mcolor.RGB$Bc<10, paste("0", mcolor.RGB$Bc, sep=""), ifelse(mcolor.RGB$Bc==100, "ff", mcolor.RGB$Bc))
+  Gc <- ifelse(mcolor.RGB$Gc<10, paste("0", mcolor.RGB$Gc, sep=""), ifelse(mcolor.RGB$Gc==100, "ff", mcolor.RGB$Gc))
+  Rc <- ifelse(mcolor.RGB$Rc<10, paste("0", mcolor.RGB$Rc, sep=""), ifelse(mcolor.RGB$Rc==100, "ff", mcolor.RGB$Rc))
+  FBGR <- paste(transp, Bc, Gc, Rc, sep="")
+  return(FBGR)
 }
 
--- branches/views/R/mix3cols.R
+++ branches/views/R/mix3cols.R
@@ -1,13 +1,31 @@
-mix3cols <-
-function(color1, color2, color3, break1, no.col) {
-# Mixes three RGB colors into a pallette
-if(missing(no.col)) { no.col <- 47 }
-if(missing(break1)) { break1 <- 1/2 }
-no.col1 <- round((break1-0)*no.col, 0)
-no.col2 <- round((1-break1)*no.col, 0)
-col1_2 <- sapply(sapply(seq(0,1,by=1/no.col1)[-no.col1], mixcolor, color1, color2), hex)
-col2_3 <- sapply(sapply(seq(0,1,by=1/no.col2)[-no.col2], mixcolor, color2, color3), hex)
-mixedcol <- c(col1_2, col2_3)
-return(mixedcol)
-}
+# R function for the plotKML package
+# Author: Tomislav Hengl
+# contact: tom.hengl@wur.nl
+# Date : July 2011
+# Version 0.1
+# Licence GPL v3
 
+mix3cols <- function(
+  color1, 
+  color2, 
+  color3, 
+  break1 = 1/2, 
+  no.col = 47
+  ){
+  # Dependencies
+  require(colorspace)
+  if(break1 <= 0 | break1 >= 1) {
+  stop('break1 must be in the (0--1) range')
+  }
+  else {
+  # round the breaks:
+  no.col1 <- round(break1*no.col, 0)
+  if(no.col1==0) {no.col1 <- no.col1+1}
+  no.col2 <- round((1-break1)*no.col, 0)
+  if(no.col2==no.col) {no.col2 <- no.col2-1}  
+  col1_2 <- sapply(sapply(seq(0,1,by=1/no.col1)[-no.col1], mixcolor, color1, color2), hex)
+  col2_3 <- sapply(sapply(seq(0,1,by=1/no.col2)[-no.col2], mixcolor, color2, color3), hex)
+  mixedcol <- c(col1_2, col2_3)
+  return(mixedcol)
+  }
+}
