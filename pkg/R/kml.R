182ffd8245581274897ff984d4b752141f01df6a
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-25 08:04:14 +0000

    Restoring changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@107 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/kml.R
+++ pkg/R/kml.R
@@ -1,27 +1,21 @@
 kml.Spatial <- function(
   obj,
   file = paste(as.character(substitute(obj, parent.frame())), ".kml", sep=""),
-  filename = file(file, "w"),
   ...,
-  overwrite = TRUE,
+  overwrite = FALSE,
   kmz = FALSE
 ){
 
-  # Open the file for writing
-  kml_open(file = file, filename = filename, name = as.character(substitute(obj, parent.frame())), overwrite = overwrite, ...)
+  kml_open(file = file, name = as.character(substitute(obj, parent.frame())), overwrite = overwrite)
 
-  kml_layer(obj = obj, file = file, filename = filename, ...)
+  kml_layer(obj = obj, file = file, ...)
 
-  kml_close(file = file, filename = filename)
-
-  # Close the file
-  close(filename)
+  kml_close(file = file)
 
   if (kmz)
     kml_compress(file)
 }
 
 setMethod("kml", "SpatialPoints", kml.Spatial)
-setMethod("kml", "SpatialGrid", kml.Spatial)
 setMethod("kml", "SpatialPolygons", kml.Spatial)
 setMethod("kml", "SpatialLines", kml.Spatial)
