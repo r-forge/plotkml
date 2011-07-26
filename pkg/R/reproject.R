d716e94904256bf75035c6295113bd6b2276864f
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-26 05:35:54 +0000

    bugfix on reproject() methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@122 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/reproject.R
+++ pkg/R/reproject.R
@@ -1,41 +1,41 @@
 .referenceCrs <- "+proj=longlat +datum=WGS84"
 
-reproject.SpatialPoints <- function(obj, crs = .referenceCrs, ...) {
-  spTransform(obj, CRS(crs))
+reproject.SpatialPoints <- function(obj, CRS = .referenceCrs, ...) {
+  spTransform(x = obj, CRSobj = CRS(CRS))
 }
 
 setMethod("reproject", "SpatialPoints", reproject.SpatialPoints)
 setMethod("reproject", "SpatialPolygons", reproject.SpatialPoints)
 setMethod("reproject", "SpatialLines", reproject.SpatialPoints)
 
-reproject.Raster <- function(obj, crs = .referenceCrs, ...) {
+reproject.Raster <- function(obj, CRS = .referenceCrs, ...) {
   if (obj@data@isfactor)
     method <- "ngb"
   else
     method <- "bilinear"
-  projectRaster(obj, crs = crs, method = method, ...)
+  projectRaster(obj, crs = CRS, method = method, ...)
 }
 
 setMethod("reproject", "Raster", reproject.Raster)
 
-reproject.SpatialPixels <- function(obj, crs = .referenceCrs, ...) {
+reproject.SpatialPixels <- function(obj, CRS = .referenceCrs, ...) {
   # SpatialPixelsDataFrame
   if ("data" %in% slotNames(obj)) {
     if (ncol(obj) > 1) {
       r <- stack(obj)
-      res <- stack(llply(r@layers, reproject, ...))
+      res <- stack(llply(r@layers, reproject, CRS = CRS, ...))
       res <- as(res, "SpatialPixelsDataFrame")
     }
     else {
       r <- raster(obj)
-      res <- as(reproject(r, ...), "SpatialPixelsDataFrame")
+      res <- as(reproject(r, CRS = CRS, ...), "SpatialPixelsDataFrame")
     }
     names(res) <- names(obj)
   }
   # SpatialPixels
   else {
     r <- raster(obj)
-    res <- as(reproject(r, ...), "SpatialPixels")
+    res <- as(reproject(r, CRS = CRS, ...), "SpatialPixels")
   }
 
   res
