10d9f1cb7caad4aadf5ed5bb66797bcbbeeaf0cb
thengl (Dr ) <tom.hengl@opengeohub.org>
2011-07-28 12:34:57 +0000

    Demo of functions (in progress)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@144 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- test_suite/demo.R
+++ test_suite/demo.R
@@ -1,6 +1,23 @@
 # will be publicly available soon !
 library(plotKML)
 
+system("Rcmd build D:/R/plotKML/working/pkg")
+# fails
+system("R CMD INSTALL D:/R/plotKML/working/pkg")
+# does not work under Windows ("cannot open file 'AAAA.R': No such file or directory")
+# Code
+source('../pkg/R/AAAA.R')
+source('../pkg/R/aesthetics.R')
+source('../pkg/R/altitude.R')
+source('../pkg/R/check_projection.R')
+source('../pkg/R/layer.SpatialPoints.R')
+source('../pkg/R/layer.SpatialPolygons.R')
+source('../pkg/R/layer.SpatialLines.R')
+source('../pkg/R/layer.Raster.R')
+source('../pkg/R/kml.R')
+source('../pkg/R/utils.R')
+source('../pkg/R/attributes.R')
+source('../pkg/R/reproject.R')
 
 ##
 ## KML authoring
@@ -19,12 +36,25 @@ proj4string(meuse.grid) <- CRS("+init=epsg:28992")
 gridded(meuse.grid) <- TRUE
 # They are not lat-long - but plotKML can take good care of it!
 
+# SpatialPolygons dataset
+nc <- readShapePoly(system.file("shapes/sids.shp", package="maptools")[1], proj4string=CRS("+proj=longlat +datum=NAD27"))
+nc.geo <- spTransform(nc, CRS("+proj=longlat +datum=WGS84"))
+
+# SpatialLines
+rm(volcano)
+volcano <- ContourLines2SLDF(contourLines(volcano))
+proj4string(volcano) <- CRS("+init=epsg:4326") # of course this is stupid
+volcano$altitude <- as.numeric(as.character(volcano$level))
+
 # Simple example: one layer
 # -------------------------
 kml(meuse.grid, colour = dist, file = "foo.kml", overwrite = TRUE)
 
 # Playing with two aesthetics in one plot (size and colour)
 kml(meuse, colour = log(zinc), size = cadmium, file = "foo.kml", overwrite = TRUE)
+# new icon:
+kml(meuse, colour = zinc, file = "foo_ball.kml", shape = "http://plotkml.r-forge.r-project.org/3Dball.png", overwrite=TRUE) 
+
 
 # More elaborated example: multi-layer
 # ------------------------------------
@@ -44,6 +74,24 @@ kml_layer(meuse, colour = soil, file = "foo.kml")
 
 # Close file
 kml_close(file = "foo.kml")
+
+# SpatialPolygons
+kml(nc.geo, '01-polygons.kml')
+library(RColorBrewer)
+kml(nc.geo, '02-polygons.kml', colour=AREA,
+colour_scale=brewer.pal(n=5, name="Blues"))
+
+# SpatialLines
+kml(volcano, file='03-lines.kml', colour=altitude, colour_scale =
+terrain.colors(10))
+
+# Multi-layer KML
+pts <- spsample(nc.geo, n = 50, type='random')
+kml_open("04-multi.kml")
+kml_layer(nc.geo, file = "04-multi.kml", colour = AREA,
+colour_scale=brewer.pal(n=5, name="Blues"))
+kml_layer(pts, file = "04-multi.kml", colour = "purple")
+kml_close("04-multi.kml")
 
 # have fun ;-)
 
