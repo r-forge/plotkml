d2e5cf139d170419993168a23b23b30de2e8ff2b
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-02 14:35:48 +0000

    Removed FWTools option for now.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@162 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/reproject.R
+++ pkg/R/reproject.R
@@ -39,11 +39,8 @@ reproject.RasterLayer <- function(obj, CRS = .referenceCrs, ...) {
 
 setMethod("reproject", "RasterLayer", reproject.RasterLayer)
 
-reproject.SpatialPixels <- function(obj, CRS = .referenceCrs, FWTools = FALSE, mvFlag = -99999, ...) {
-  # Locate GDALwarp:
-  fw.path <- shortPathName(readRegistry("SOFTWARE\\WOW6432NODE\\FWTools")$Install_Dir)
-  gdalwarp <- ifelse(.Platform$OS.type=="windows", paste(fw.path, "bin", "gdalwarp.exe", sep="\\"), "gdalwarp")
-  
+reproject.SpatialPixels <- function(obj, CRS = .referenceCrs, ...) {
+    
   # SpatialPixelsDataFrame
   if ("data" %in% slotNames(obj)) {
     i_nm <- which(sapply(obj@data, class) == 'numeric')
@@ -51,25 +48,9 @@ reproject.SpatialPixels <- function(obj, CRS = .referenceCrs, FWTools = FALSE, m
     # if multiple layers:
     if (ncol(obj) > 1) {
 
-    if(FWTools==TRUE){
-       for(i in 1:length(ncol(obj))){
-       writeGDAL(obj[i], paste(names(obj)[i], ".tif", sep=""), "GTiff", mvFlag=mvFlag)
-       unlink(paste(names(obj)[i], "_ll.sdat", sep=""))
-       # reproject/resample using FWTools:
-       cmd <- paste(gdalwarp, " ", names(obj)[i], ".tif ", names(obj)[i], "_ll.sdat -of \"SAGA\" -r bilinear -s_srs \"", proj4string(obj), "\" -t_srs \"+proj=longlat +datum=WGS84\" -srcnodata ", mvFlag, " -dstnodata -99999", sep="")
-       sss <- try(system(cmd), silent=TRUE)
-      }
-      res <- readGDAL(paste(names(obj)[1], "_ll.sdat", sep=""))
-      names(res) <- names(obj)[i]
-      for(i in 2:length(ncol(obj))){ res@data[,names(obj)[i]] <- readGDAL(paste(names(obj)[i], "_ll.sdat", sep=""))$band1  }
-       
-      }
-      else{
-
       r <- stack(obj)
       res <- stack(llply(r@layers, reproject, CRS = CRS, ...))
       res <- as(res, "SpatialPixelsDataFrame")
-    }
     }
     
     else {
