fdb8d7b3b1653442802904b91f0ffdead603e96c
thengl (Dr ) <tom.hengl@opengeohub.org>
2011-08-02 12:06:16 +0000

    Added functionality from the "animation" package which can automatically locate ImageMagick on multiple-platforms.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@161 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/kml_legend.R
+++ pkg/R/kml_legend.R
@@ -74,9 +74,10 @@ kml_legend <- function(
 
 # Generate PNG legend for whitening (2D)
 #
-kml_legend.whitening <- function(legend.res = 0.01, width=120, height=300, pointsize = 14, x.lim, e.lim, leg.asp = height/width, legend.file = "whitening_legend.png", matte="") {
+kml_legend.whitening <- function(legend.res = 0.01, width=120, height=300, pointsize = 14, x.lim, e.lim, leg.asp = 0.3*width/height, legend.file = "whitening_legend.png", matte="") {
   require(colorspace)
   require(pixmap)
+  require(animation)
    
   xlg <- seq(.01,1,by=legend.res)
   ylg <- seq(.01,1,by=legend.res)
@@ -94,17 +95,17 @@ kml_legend.whitening <- function(legend.res = 0.01, width=120, height=300, point
   leg.plt <- pixmapIndexed(data=1:length(HSV), nrow=length(ylg), ncol=length(xlg), bbox=c(e.lim[1], x.lim[1], e.lim[2], x.lim[2]), col=HSV)
   # par(las = 0)
   png(file=legend.file, width=width, height=height, bg="transparent", pointsize=pointsize)
-  par(mar=c(3.5,2.5,1,0))
-  plot(leg.plt, axes=FALSE, col.lab=rgb(0.99,0.99,0.99), bg=NA, asp=1/leg.asp)
+  par(mar=c(2.5,2.5,0.5,0))
+  plot(leg.plt, axes=FALSE, col.lab=rgb(0.99,0.99,0.99), bg=NA, asp=leg.asp)
   axis(side=1, at=e.lim, cex=.8, col.axis=rgb(0.99,0.99,0.99), col.lab=rgb(0.99,0.99,0.99))
   axis(side=2, at=signif(x.lim, 3), cex=.8, col.axis=rgb(0.99,0.99,0.99), col.lab=rgb(0.99,0.99,0.99))
   dev.off()
  
   ## Force transparency (requires ImageMagick):
  	if(matte!= "") {
-    im.path <- shortPathName(readRegistry("SOFTWARE\\ImageMagick\\Current")$BinPath)
- 	  convert <- ifelse(.Platform$OS.type=="windows", paste(im.path, "convert.exe", sep="\\"), paste("/usr/bin", "convert", sep="/"))
-		cmd <- paste(convert, ' ', legend.file, ' -matte -transparent "#FFFFFF" ', legend.file, sep="")
-		sss <- try(system(cmd, intern=TRUE), silent=TRUE)
+    # get the path of ImageMagick using the animation package:
+    im.convert <- ani.options('convert')
+    cmd <- paste(im.convert, ' ', legend.file, ' -matte -transparent "#FFFFFF" ', legend.file, sep="")
+    sss <- try(system(cmd, intern=TRUE), silent=TRUE) 
    }
 }
