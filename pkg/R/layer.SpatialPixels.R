c999a199ec8e762406959a57f9644c9120ab17d8
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-05 06:01:32 +0000

    SpatialPixels are now coerced into Raster* object - to light up the code, we use the raster* method.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@180 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/layer.SpatialPixels.R
+++ pkg/R/layer.SpatialPixels.R
@@ -7,96 +7,15 @@ kml_layer.SpatialPixels <- function(
   LabelScale = 0.7,
   ...
   ){
-  
-  # get our invisible file connection from custom evnrionment
-  file.connection <- get('kml.file.out', env=plotKML.fileIO)
 
-  # Checking the projection is geo
-  check <- check_projection(obj, logical = TRUE)
-
-  # Trying to reproject data if the check was not successful
-  if (!check)
-    obj <- reproject(obj)
-
-  # Parsing the call for "colour"
-  call <- substitute(list(...))
-  call <- as.list(call)[-1]
-
-  # Parse the current call
-  colour <- charmatch("colour", names(call))
-  if (is.na(colour))
-    stop("No attribute to map. Please use the colour=... option.")
+  # converting object to Raster*
+  if (ncol(obj) > 1)
+    obj <- as(obj, "RasterStack")
   else
-    data <- eval(call[["colour"]], envir = obj@data)
+    obj <- as(obj, "RasterLayer")
 
-  altitude <- eval(call[["altitude"]], obj@data)
-  altitude <- kml_altitude(obj, altitude = altitude)
-  altitudeMode <- kml_altitude_mode(altitude)
+  kml_layer(obj, ...)
 
-  pal <- charmatch("colour_scale", names(call))
-  if (!is.na(pal))
-    pal <- eval(call[["colour_scale"]])
-  else {
-    if (is.numeric(data))
-      pal <- .colour_scale_numeric
-    else
-      pal <- .colour_scale_factor
-  }
-
-  colour_scale <- colorRampPalette(pal)(length(data))
-
-  # Transparency
-  alpha <- charmatch("alpha", names(call))
-  if (!is.na(alpha)) {
-    colour_scale <- kml_alpha(obj, alpha = eval(call[["alpha"]], obj@data), colours = colour_scale, RGBA = TRUE)
-  }
-
-  # Creating a SpatialPixelsDataFrame object to be plotted
-  call_name <- deparse(call[["colour"]])
-  data <- data.frame(data)
-  names(data) <- call_name
-
-  # Building image object for PNG generation
-  spdf <- SpatialPixelsDataFrame(points = coordinates(obj), data = data)
-  img <- raster(spdf, layer = 1)
-
-  # Creating the PNG file: creation of the file name may not always work...
-  raster_name <- paste(summary(file.connection)$description, ".png", sep = "")
-
-  # Plotting the image
-  png(file = raster_name, bg = "transparent") # , width = grd$width, height = grd$height)
-  par(mar = c(0, 0, 0, 0), xaxs = "i", yaxs = "i")
-  image(img, col = colour_scale, frame.plot = FALSE)
-  dev.off()
-
-  # Folder and name of the points folder
-  cat("<Folder>\n", file = file.connection, append = TRUE)
-  cat("<name>", call_name, "</name>\n", sep = "", file = file.connection, append = TRUE)
-
-  # write into the a KML file:
-  write('<GroundOverlay>', file.connection, append = TRUE)
-  write(paste('<name>', call_name, '</name>', sep=""), file.connection, append = TRUE)
-
-  write(paste('<altitude>', altitude, '</altitude>', sep = ""), file.connection, append = TRUE)
-  write(paste('<altitudeMode>', altitudeMode, '</altitudeMode>', sep = ""), file.connection, append = TRUE)
-
-  # Using the previously generated raster file
-  write('<Icon>', file.connection, append = TRUE)
-  write(paste('<href>', raster_name, '</href>', sep = ""), file.connection, append = TRUE)
-  write('</Icon>', file.connection, append = TRUE)
-
-  # Bounding box used to drape the raster file
-  write('<LatLonBox>', file.connection, append = TRUE)
-  write(paste('<north>', bbox(obj)[2, 2], '</north>', sep = ""), file.connection, append = TRUE)
-  write(paste('<south>', bbox(obj)[2, 1], '</south>', sep = ""), file.connection, append = TRUE)
-  write(paste('<east>', bbox(obj)[1, 2], '</east>', sep = ""), file.connection, append = TRUE)
-  write(paste('<west>', bbox(obj)[1, 1], '</west>', sep = ""), file.connection, append = TRUE)
-  write('</LatLonBox>', file.connection, append = TRUE)
-
-  write('</GroundOverlay>', file.connection, append = TRUE)
-
-  # Closing the folder
-  cat("</Folder>\n", file = file.connection, append = TRUE)
 }
 
 setMethod("kml_layer", "SpatialPixels", kml_layer.SpatialPixels)
