f5b69749cd0cdd008398c06cbb9bcea96d22a718
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-18 07:36:53 +0000

    Added color palettes and icon list to the plotKML homepage.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@229 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -22,6 +22,20 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
     <style type="text/css">
 <!--
 .style1 {font-size: x-small}
+.R_code {
+font-family:"Courier New", Courier, monospace;
+font-size: x-small
+}
+.R_env {
+font-family:"Courier New", Courier, monospace;
+color:#0000FF;
+font-size: x-small
+}
+.R_arg {
+font-family:"Courier New", Courier, monospace;
+color:#FF0000;
+font-size: x-small;
+}
 -->
     </style>
 </head>
@@ -53,16 +67,70 @@ echo $contents; } ?>
 </div>
 <table width="600" border="0" cellspacing="0" cellpadding="10">
   <tr>
-    <td><div align="center"><a href="http://globalsoilmap.net/data/plotKML_examples.zip"><img src="Fig_plotKML_bubble_1_s.jpg" alt="Bubble plot (points)" width="198" height="200" border="0" /></a></div></td>
-    <td><a href="http://globalsoilmap.net/content/animated-display-uncertainty-mapping-soil-classes"><img src="Fig_plotKML_anime_1_s.jpg" alt="Animations using spatial grids" width="162" height="200" border="0" /></a></td>
-    <td><div align="center"><a href="http://globalsoilmap.net/content/prediction-or-organic-matter-geo-gif-kml"><img src="Fig_plotKML_grid_om_s.jpg" alt="Results of spatial prediction" width="174" height="200" border="0" /></a></div></td>
-    <td><div align="center"><a href="http://globalsoilmap.net/data/plotKML_examples.zip"><img src="Fig_plotKML_geopath_s.jpg" alt="Geopath (between two points)" width="171" height="200" border="0" /></a></div></td>
-    <td><div align="center"><a href="http://globalsoilmap.net/content/soil-horizons-and-values-organic-carbon-visualized-google-earth"><img src="Fig_plotKML_soilblock_s.jpg" alt="Soil profiles as 3D blocks" width="170" height="200" border="0" /></a></div></td>
+    <td valign="top"><div align="center">
+      <p><a href="http://globalsoilmap.net/data/plotKML_examples.zip"><img src="Fig_plotKML_bubble_1_s.jpg" alt="Bubble plot (points)" width="198" height="200" border="0" /></a></p>
+      <p class="R_code"><strong>kml</strong>(meuse, file = <span class="R_arg">&quot;meuse_zinc.kml&quot;</span>, <span class="R_env"> size=</span>zinc, <span class="R_env">label</span>=zinc) </p>
+    </div></td>
+    <td valign="top"><a href="http://globalsoilmap.net/content/animated-display-uncertainty-mapping-soil-classes"><img src="Fig_plotKML_anime_1_s.jpg" alt="Animations using spatial grids" width="162" height="200" border="0" /></a></td>
+    <td valign="top"><div align="center"><a href="http://globalsoilmap.net/content/prediction-or-organic-matter-geo-gif-kml"><img src="Fig_plotKML_grid_om_s.jpg" alt="Results of spatial prediction" width="174" height="200" border="0" /></a></div></td>
+    <td valign="top"><div align="center"><a href="http://globalsoilmap.net/data/plotKML_examples.zip"><img src="Fig_plotKML_geopath_s.jpg" alt="Geopath (between two points)" width="171" height="200" border="0" /></a></div></td>
+    <td valign="top"><div align="center"><a href="http://globalsoilmap.net/content/soil-horizons-and-values-organic-carbon-visualized-google-earth"><img src="Fig_plotKML_soilblock_s.jpg" alt="Soil profiles as 3D blocks" width="170" height="200" border="0" /></a></div></td>
   </tr>
 </table>
 <p class="style1">Contact: <a href="http://www.wewur.wur.nl/popups/vcard.aspx?id=HENGL001" target="_blank">Tomislav Hengl</a>, <a href="http://www.landcareresearch.co.nz/research/staff_page.asp?staff_num=2132" target="_blank">Pierre Roudier</a> &amp; <a href="http://casoilresource.lawr.ucdavis.edu/drupal/node/905" target="_blank">Dylan Beaudette</a></p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>.</p>
-
+<p class="style1">Poster at UseR 2011: <a href="poster-plotKML-UseR2011.pdf">&quot;plotKML: a framework for visualization of space-time data&quot;</a> (PDF) </p>
+<hr />
+<table width="500" border="0" cellspacing="5" cellpadding="10">
+  <caption class="style1">
+    Some common plotKML icons
+  </caption>
+  <tr>
+    <td class="R_arg"><div align="center">cross</div></td>
+    <td class="R_arg"><div align="center">3Dball</div></td>
+    <td class="R_arg"><div align="center">3Dballred</div></td>
+    <td class="R_arg"><div align="center">3Dballyellow</div></td>
+    <td class="R_arg"><div align="center">circle</div></td>
+    <td class="R_arg"><div align="center">hexagon</div></td>
+    <td class="R_arg"><div align="center">triangle</div></td>
+    <td class="R_arg"><div align="center">square</div></td>
+    <td class="R_arg"><div align="center">golfhole</div></td>
+    <td class="R_arg"><div align="center">pointer</div></td>
+  </tr>
+  <tr>
+    <td>      <p align="center"><img src="cross.png" alt="cross.png" width="45" height="45" /><br />
+      </p></td>
+    <td><div align="center"><img src="3Dball.png" alt="3Dball.png" width="45" height="45" /></div></td>
+    <td><div align="center"><img src="3Dballred.png" alt="3Dballred" width="45" height="45" /></div></td>
+    <td><div align="center"><img src="3Dballyellow.png" alt="3Dballyellow" width="45" height="45" /></div></td>
+    <td><div align="center"><img src="circle.png" alt="circle.png" width="45" height="45" /></div></td>
+    <td><div align="center"><img src="hexagon.png" alt="hexagon.png" width="45" height="52" /></div></td>
+    <td><div align="center"><img src="triangle.png" alt="triangle.png" width="55" height="48" /></div></td>
+    <td><div align="center"><img src="square.png" alt="square.png" width="50" height="50" /></div></td>
+    <td><div align="center"><img src="golfhole.png" alt="glofhole.png" width="45" height="63" /></div></td>
+    <td><div align="center"><img src="pointer.png" alt="pointer.png" width="35" height="65" /></div></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<hr />
+<table width="500" border="0" cellspacing="5" cellpadding="10" summary="To load a color pallete use "data(saga_pal)="data(saga_pal)"" method.">
+  <caption class="style1">
+    Color palettes
+  </caption>
+  <tr>
+    <td><div align="center" class="R_arg">SAGA_pal[['n']]</div></td>
+    <td><div align="center"><span class="R_arg">R_pal[['n']]</span></div></td>
+    <td><div align="center"><span class="R_arg">worldgrids_pal[['n']]</span></div></td>
+  </tr>
+  <tr>
+    <td><div align="center"><img src="SAGA_pal.png" alt="SAGA_pal.png" width="238" height="1020" /></div></td>
+    <td><div align="center"><img src="R_pal.png" alt="R_pal.png" width="238" height="1020" /></div></td>
+    <td><div align="center"><img src="worldgrids_pal.png" alt="worldgrids_pal.png" width="238" height="1020" /></div></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<hr />
+<p>&nbsp;</p>
 </body>
 </html>
