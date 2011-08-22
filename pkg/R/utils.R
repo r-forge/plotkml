c53a5af1f2d15430aecc483d88d9f6dcc4fa64a3
thengl (Dr ) <tom.hengl@opengeohub.org>
2011-08-22 07:26:20 +0000

    Added some customized R color palettes (functions).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@234 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -166,3 +166,31 @@ display.pal <- function(pal, sel=1:10, names=FALSE) {
 	mtext(pal.name, cex=.8, side=3)  
   }
 }
+
+
+## R color palettes
+# Standard soil vars color legends
+# e.g. soil organic carbon:
+soc.pal <- mix5cols(color1=RGB(0.706, 0.804, 0.804), color2=RGB(0.663, 0.663, 0.663), color3=RGB(0, 0.392, 0), color4=RGB(0.0,0.1,0.0), color5=RGB(0,0,0), break1=.2, break2=.4, break3=.6, no.col=20)
+# soil pH:
+pH.pal <- hsv(seq(0,1,by=1/27))[1:20]
+# texture fractions:
+tex.pal <- mix3cols(color1=RGB(1,0.843,0), color2=RGB(0.5451,0.353,0.169), color3=RGB(0,0,0), break1=.6, no.col=20)
+## HCL examples from [http://cran.r-project.org/web/packages/colorspace/vignettes/hcl-colors.pdf]
+blue_grey_red <- diverge_hcl(20, c = c(100, 0), l = c(50, 90), power = 1.3)
+grey_black <- rev(sequential_hcl(20, c = 0, power = 2.2))
+
+# Other popular R palletes for continuous variables:
+R_pal <- NULL
+R_pal[[1]] <- rev(rainbow(27)[1:20]) 
+R_pal[[2]] <- rev(heat.colors(20))
+R_pal[[3]] <- terrain.colors(20)
+R_pal[[4]] <- topo.colors(20)
+R_pal[[5]] <- rev(bpy.colors(20))
+R_pal[[6]] <- soc.pal
+R_pal[[7]] <- pH.pal
+R_pal[[8]] <- tex.pal
+R_pal[[9]] <- blue_grey_red
+R_pal[[10]] <- grey_black
+
+names(R_pal) <- c("rainbow_75", "heat_colors", "terrain_colors", "topo_colors", "bpy_colors", "soc_pal", "pH_pal", "tex_pal", "blue_grey_red", "grey_black")
