b02fc0d2e4bad62e3faa3fef8c7de95bf428bfc7
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-12 23:43:18 +0000

    adding flexible balloon parsing... and introduced a bug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@244 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/attributes.R
+++ pkg/R/attributes.R
@@ -1,25 +1,31 @@
 # convert an attribute table associated with spatial data into an HTML <description> bubble
 # for use within Google Earth
-.df_to_kml_html_table <- function(df, n_digits_max = 3) {
+.df_to_kml_html_table <- function(x, columns=TRUE) {
   
   # get our invisible file connection from custom evnrionment
   file.connection <- get('kml.file.out', env=plotKML.fileIO)
   
-  # Rounding the numeric fields for display
-  df <- round(df[which(sapply(df, class) == "numeric")], digits = n_digits_max)
+  
+  # if the user passed in TRUE, then we want all of the columns
+  if(class(columns) == 'logical')
+      columns <- 1:ncol(x) # use all columns from the dataframe row
+  
+  # otherwise, keep only requested columns
+  x <- x[, columns]
 
   # empty list to store html elements
   html.list <- list()
 
   # create header
-  table.head <- c('<tr style="background-color: #D3D3D3; font-weight: bold; text-align: center; color: #000000;">', paste('<td>', names(df), '</td>', sep=''), "</tr>")
+  table.head <- c('<tr style="background-color: #D3D3D3; font-weight: bold; text-align: center; color: #000000;">', paste('<td>', names(x), '</td>', sep=''), "</tr>\n")
 
   # create the body
-  table.body <- apply(df, 1, .df_row_to_html_row)
-
+  table.body <- paste(sapply(x, function(i) { paste('<td style="width:auto;">', as.character(i), '</td>', sep = '') }), collapse='')
+  table.body <- paste('<tr style="background-color: #E9E9E9; color: #000000;">', table.body, "</tr>", collapse = '')
+    
   # make the table
   html.list <- append(html.list, '<description><![CDATA[')
-  html.list <- append(html.list, '<table border="0" padding="2" spacing="1">')
+  html.list <- append(html.list, '<table border="0" padding="2" spacing="1" style="width:600px;>')
   html.list <- append(html.list, paste(table.head, collapse = ''))
   html.list <- append(html.list, paste(table.body, collapse = ''))
   html.list <- append(html.list, '</table>')
@@ -28,6 +34,4 @@
   cat(paste(html.list, collapse = "\n"), file = file.connection, append = TRUE)
 }
 
-# convert each DF row into a table row and apply default styling
-.df_row_to_html_row <- function(x)
-  paste(c('<tr style="background-color: #E9E9E9; color: #000000;">', paste('<td>', x, '</td>', sep = ''), '</tr>'), collapse = '')
+
