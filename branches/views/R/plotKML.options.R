ab9705a15042969866ad23849c1827d9e30953fd
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-15 02:12:36 +0000

    Automatically looks for external applications.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@247 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/views/R/plotKML.options.R
+++ branches/views/R/plotKML.options.R
@@ -1,47 +1,48 @@
 # Purpose        : Various utilitis for plotKML
-# Maintainer     : Dylan Beaudette (debeaudette@ucdavis)
-# Contributions  : Tomislav Hengl (tom.hengl@wur.nl).edu); Pierre Roudier (pierre.roudier@landcare.nz)
+# Maintainer     : Tomislav Hengl (tom.hengl@wur.nl)
+# Contributions  : Dylan Beaudette (debeaudette@ucdavis.edu); Pierre Roudier (pierre.roudier@landcare.nz)
 # Status         : not tested yet
-# Note           : This function should reduce all possible incompatibilities and conflicts
+# Note           : This function helps establishing a link with external packages and settings
 
 
 ## looks for FWTools in the Windows Registry Hive,
 ## the Program Files directory and the usr/bin installation
 
-# plotKML.options = function(kml.schema.uri, kml.simpleType, kml.complexType, convert, gdalwarp = "", gdal_translate = "") {
-### This function needs fixing...
-   
-#    require(animation)
-#    require(XML)
-#   # xsd <- system.file("ogckml22.xsd", package = "plotKML")
-#   xsd <- "http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd"
-#   doc <- xmlTreeParse(xsd, isURL=TRUE) # The package should load this from a local file;
-#   tmp <- xmlRoot(doc)
-#   dd <-  xmlNamespaceDefinitions(tmp, recursive = TRUE)
-#   sel <- tmp[which(names(xmlChildren(tmp))=="complexType")]
-#   sel2 <- tmp[which(names(xmlChildren(tmp))=="simpleType")]
-#   kml.schema.uri = dd$kml$uri
-#   kml.simpleType = sapply(sel2, xmlGetAttr, "name") 
-#   kml.complexType = sapply(sel, xmlGetAttr, "name")
-#   convert = ani.options('convert')
-#   .plotKML.opts <- list(kml.schema.uri, kml.simpleType, kml.complexType, convert, gdalwarp, gdal_translate)
-#   return(.plotKML.opts)
-# }
-
-
 ### Try to locate FWTools under various platforms:
-fw.path <- function(gdalwarp = "", gdal_translate = ""){
-  # Under Linux make sure you add path to gdalwarp by using e.g.:
-  # export PATH=INSTALL_DIRECTORY/FWTools-2.*.*/bin_safe:$PATH
-  if (.Platform$OS.type == "windows") {
-     if (!inherits(try({ fw.path = utils::readRegistry("SOFTWARE\\WOW6432NODE\\FWTools")$Install_Dir }, silent = TRUE), "try-error")) 
+paths <- function(gdalwarp = "", gdal_translate = "", convert = ""){
+    # Under Linux make sure you add path to gdalwarp by using e.g.:
+    # export PATH=INSTALL_DIRECTORY/FWTools-2.*.*/bin_safe:$PATH
+  
+     require(animation)
+     convert <- ani.options("convert")
+     
+    if(is.null(convert)){
+        if(.Platform$OS.type == "windows") {
+        if(nzchar(grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ";")[[1]], pattern="Magick"))) {
+        im.dir <- paths[grep(paths, pattern="ImageMagick")[1]]
+        convert = shQuote(normalizePath(file.path(im.dir, "convert.exe")))
+        message(paste("Located ImageMagick from the path: \"", im.dir, "\"", sep=""))
+        }}
+        else{
+        if(nzchar(grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ":")[[1]], pattern="Magick"))) {
+        im.dir <- paths[grep(paths, pattern="Magick")[1]]
+        convert = "convert"
+        message(paste("Located ImageMagick from the path: \"", im.dir, "\"", sep=""))
+    }}}
+    else { 
+    warning("Install ImageMagick and add to PATH. See http://imagemagick.org for more info.")
+      }  
+  
+  if(.Platform$OS.type == "windows") {
+
+     if(!inherits(try({ fw.path = utils::readRegistry("SOFTWARE\\WOW6432NODE\\FWTools")$Install_Dir }, silent = TRUE), "try-error")) 
       { if (nzchar(fw.path))  { 
       gdalwarp = shQuote(normalizePath(file.path(fw.path, "bin/gdalwarp.exe")))
       gdal_translate = shQuote(normalizePath(file.path(fw.path, "bin/gdal_translate.exe")))
       message(paste("Located FWTools from the Registry Hive: \"", fw.path, "\"", sep="")) 
               }
       }
-      else if( nzchar(prog <- Sys.getenv("ProgramFiles")) &&
+      else if(nzchar(prog <- Sys.getenv("ProgramFiles")) &&
           length(fw.dir <- list.files(prog, "^FWTools.*")) &&
           length(fw.path <- list.files(file.path(prog, fw.dir), pattern = "^gdalwarp\\.exe$", full.names = TRUE, recursive = TRUE))  |
           length(fw.path2 <- list.files(file.path(prog, fw.dir), pattern = "^gdal_translate\\.exe$", full.names = TRUE, recursive = TRUE)) )
@@ -64,9 +65,11 @@ fw.path <- function(gdalwarp = "", gdal_translate = ""){
       warning("Could not locate FWTools! Install program and add it to the Windows registry. See http://fwtools.maptools.org for more info.")
       return()
        }
-    } 
+    }
+    
     else {
-    if ( nzchar(grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ":")[[1]], pattern="FWTools")) ) {
+    
+    if(nzchar(grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ":")[[1]], pattern="FWTools"))) {
     fw.dir <- paths[grep(paths, pattern="FWTools")[1]]
     gdalwarp = "gdalwarp"
     gdal_translate = "gdal_translate"
@@ -74,11 +77,49 @@ fw.path <- function(gdalwarp = "", gdal_translate = ""){
       }
     else { 
     warning("Install FWTools and add to PATH. See http://fwtools.maptools.org for more info.")
-                    return()
       }
     }
-    ## write it into plotKML.options() to save future efforts
-    # plotKML.options(gdalwarp = gdalwarp, gdal_translate = gdal_translate)
-    return(gdalwarp)
-    return(gdal_translate)
+
+    lt <- data.frame(gdalwarp, gdal_translate, convert, stringsAsFactors = FALSE)
+    return(lt)
 }
+
+## Standard settings:
+plotKML.env <- function(
+    colour_scale_numeric,
+    colour_scale_factor,
+    xsd,
+    convert,
+    gdalwarp,
+    gdal_translate,
+    googleAPIkey
+    ){
+    
+    require(RColorBrewer)
+    
+    plotKML.opts <<- new.env(hash=TRUE)
+    
+    if(missing(colour_scale_numeric)) { colour_scale_numeric <- rev(brewer.pal(n = 5, name = "Spectral")) }
+    if(missing(colour_scale_factor)) { colour_scale_factor <- brewer.pal(n = 6, name = "Set1") }
+    if(missing(xsd)) { xsd <- "http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd" }
+    fw <- paths()
+    if(missing(convert)) { convert <- fw$convert[[1]] }
+    if(missing(gdalwarp)) { gdalwarp <- fw$gdalwarp[[1]] }
+    if(missing(gdal_translate)) { gdal_translate <- fw$gdal_translate[[1]] }
+    if(missing(googleAPIkey)) { googleAPIkey <- "ABQIAAAAqHzabFRj8QwDECupLUR4-hT53Nvo2rq6JtI9-sNzq2yJTiKUYBRN5VP8pfrIcMaRo0pNDvBhWJUQCA" }
+ 
+    assign("colour_scale_numeric", colour_scale_numeric, envir=plotKML.opts)
+    assign("colour_scale_factor", colour_scale_factor, envir=plotKML.opts)
+    assign("xsd", xsd, envir=plotKML.opts)
+    assign("convert", convert, envir=plotKML.opts)
+    assign("gdalwarp", gdalwarp, envir=plotKML.opts)
+    assign("gdal_translate", gdal_translate, envir=plotKML.opts)
+    assign("googleAPIkey", googleAPIkey, envir=plotKML.opts)
+    
+    .plotKML.opts <- list(colour_scale_numeric, colour_scale_factor, xsd, convert, gdalwarp, gdal_translate, googleAPIkey)
+    names(.plotKML.opts) <- c("colour_scale_numeric", "colour_scale_factor", "xsd", "convert", "gdalwarp", "gdal_translate", "googleAPIkey")
+    return(.plotKML.opts)
+}
+
+## Optional: put "library(plotKML); plotKML.env(...)" in your "/etc/Rprofile.site"
+
