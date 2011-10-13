be75574db4b9c9d482b4f74639ad80f2c0c37669
thengl (Dr ) <tom.hengl@opengeohub.org>
2011-10-13 14:42:02 +0000

    plotKML options almost complete.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@254 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/views/R/plotKML.options.R
+++ branches/views/R/plotKML.options.R
@@ -1,45 +1,53 @@
 # Purpose        : Various utilitis for plotKML
 # Maintainer     : Tomislav Hengl (tom.hengl@wur.nl)
 # Contributions  : Dylan Beaudette (debeaudette@ucdavis.edu); Pierre Roudier (pierre.roudier@landcare.nz)
-# Status         : not tested yet
+# Status         : tested and ready for CRAN
 # Note           : This function helps establishing a link with external packages and settings
 
 
-## looks for FWTools in the Windows Registry Hive,
-## the Program Files directory and the usr/bin installation
+## looks for FWTools in the Windows Registry Hive, the Program Files directory and the usr/bin installation
 
 ### Try to locate FWTools under various platforms:
-paths <- function(gdalwarp = "", gdal_translate = "", convert = ""){
+paths <- function(gdalwarp = "", gdal_translate = "", convert = "", saga_cmd = "", show.paths = FALSE){
     # Under Linux make sure you add path to gdalwarp by using e.g.:
     # export PATH=INSTALL_DIRECTORY/FWTools-2.*.*/bin_safe:$PATH
   
+     require(RSAGA)
      require(animation)
      convert <- ani.options("convert")
+     saga_cmd <- shortPathName(normalizePath(paste(rsaga.env()$path, rsaga.env()$cmd, sep="/")))
+     saga.version <- rsaga.get.version()
      
     if(is.null(convert)){
         if(.Platform$OS.type == "windows") {
-        if(nzchar(grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ";")[[1]], pattern="Magick"))) {
+        if(!length(x <- grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ";")[[1]], pattern="Magick"))==0) {
         im.dir <- paths[grep(paths, pattern="ImageMagick")[1]]
         convert = shQuote(normalizePath(file.path(im.dir, "convert.exe")))
-        message(paste("Located ImageMagick from the path: \"", im.dir, "\"", sep=""))
+        message(system(convert,  show.output.on.console = FALSE, intern = TRUE)[1])
+        # message(paste("Located ImageMagick from the path: \"", im.dir, "\"", sep=""))
         }}
         else{
-        if(nzchar(grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ":")[[1]], pattern="Magick"))) {
+        if(!length(x <- grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ":")[[1]], pattern="Magick"))==0) {
         im.dir <- paths[grep(paths, pattern="Magick")[1]]
         convert = "convert"
-        message(paste("Located ImageMagick from the path: \"", im.dir, "\"", sep=""))
-    }}}
+        message(system(convert,  show.output.on.console = FALSE, intern = TRUE)[1])
+        # message(paste("Located ImageMagick from the path: \"", im.dir, "\"", sep=""))
+    }}
+        if(is.null(im.dir)){ 
+        warning("Install ImageMagick and add to PATH. See http://imagemagick.org for more info.")
+        }
+    }
     else { 
-    warning("Install ImageMagick and add to PATH. See http://imagemagick.org for more info.")
+    message(system(convert,  show.output.on.console = FALSE, intern = TRUE)[1])
       }  
   
   if(.Platform$OS.type == "windows") {
 
      if(!inherits(try({ fw.path = utils::readRegistry("SOFTWARE\\WOW6432NODE\\FWTools")$Install_Dir }, silent = TRUE), "try-error")) 
       { if (nzchar(fw.path))  { 
-      gdalwarp = shQuote(normalizePath(file.path(fw.path, "bin/gdalwarp.exe")))
-      gdal_translate = shQuote(normalizePath(file.path(fw.path, "bin/gdal_translate.exe")))
-      message(paste("Located FWTools from the Registry Hive: \"", fw.path, "\"", sep="")) 
+      gdalwarp = shQuote(shortPathName(normalizePath(file.path(fw.path, "bin/gdalwarp.exe"))))
+      gdal_translate = shQuote(shortPathName(normalizePath(file.path(fw.path, "bin/gdal_translate.exe"))))
+      message(paste("Located FWTools from the Registry Hive: \"", shortPathName(fw.path), "\"", sep="")) 
               }
       }
       else if(nzchar(prog <- Sys.getenv("ProgramFiles")) &&
@@ -47,41 +55,81 @@ paths <- function(gdalwarp = "", gdal_translate = "", convert = ""){
           length(fw.path <- list.files(file.path(prog, fw.dir), pattern = "^gdalwarp\\.exe$", full.names = TRUE, recursive = TRUE))  |
           length(fw.path2 <- list.files(file.path(prog, fw.dir), pattern = "^gdal_translate\\.exe$", full.names = TRUE, recursive = TRUE)) )
        {
-      gdalwarp = shQuote(normalizePath(fw.path[1]))
-      gdal_translate = shQuote(normalizePath(fw.path2[1]))
-      message(paste("Located FWTools from the 'Program Files' directory: \"", fw.path, "\"", sep=""))
+      gdalwarp = shQuote(shortPathName(normalizePath(fw.path[1])))
+      gdal_translate = shQuote(shortPathName(normalizePath(fw.path2[1])))
+      message(paste("Located FWTools from the 'Program Files' directory: \"", shortPathName(fw.path), "\"", sep=""))
      }
      else if (nzchar(prog <- Sys.getenv("ProgramFiles(x86)")) &&
           length(fw.dir <- list.files(prog, "^FWTools.*")) &&
           length(fw.path <- list.files(file.path(prog, fw.dir), pattern = "^gdalwarp\\.exe$", full.names = TRUE, recursive = TRUE))  &&
           length(fw.path2 <- list.files(file.path(prog, fw.dir), pattern = "^gdal_translate\\.exe$", full.names = TRUE, recursive = TRUE))   )
        {
-      gdalwarp = shQuote(normalizePath(fw.path[1]))
-      gdal_translate = shQuote(normalizePath(fw.path2[1]))
-      message(paste("Located FWTools from the 'Program Files' directory: \"", fw.path, "\"", sep=""))
+      gdalwarp = shQuote(shortPathName(normalizePath(fw.path[1])))
+      gdal_translate = shQuote(shortPathName(normalizePath(fw.path2[1])))
+      message(paste("Located FWTools from the 'Program Files' directory: \"", shortPathName(fw.path), "\"", sep=""))
      }
      
      else {
       warning("Could not locate FWTools! Install program and add it to the Windows registry. See http://fwtools.maptools.org for more info.")
       return()
        }
-    }
+       
+       if(is.null(saga_cmd)){
+       if(nzchar(prog <- Sys.getenv("ProgramFiles")) &&
+          length(saga.dir <- list.files(prog, "^SAGA*"))>0 &&
+          length(saga_cmd <- list.files(file.path(prog, saga.dir), pattern = "^saga_cmd\\.exe$", full.names = TRUE, recursive = TRUE))>0  
+          )
+       {
+      myenv <- rsaga.env(path=shQuote(normalizePath(saga.dir[1])))
+      saga_cmd <- shortPathName(normalizePath(paste(myenv$path, myenv$cmd, sep="/")))
+      saga.version <- myenv$version 
+      message(paste("Located SAGA GIS ", saga.version, " from the 'Program Files' directory: \"", shortPathName(saga_cmd), "\"", sep=""))
+     }
+     else if (nzchar(prog <- Sys.getenv("ProgramFiles(x86)")) &&
+          length(saga.dir <- list.files(prog, "^SAGA*"))>0 &&
+          length(saga_cmd <- list.files(file.path(prog, saga.dir), pattern = "^saga_cmd\\.exe$", full.names = TRUE, recursive = TRUE))>0   
+          )
+       {
+      myenv <- rsaga.env(path=shQuote(normalizePath(saga.dir[1])))
+      saga_cmd <- shortPathName(normalizePath(paste(myenv$path, myenv$cmd, sep="/")))
+      saga.version <- myenv$version 
+      message(paste("Located SAGA GIS ", saga.version, " from the 'Program Files' directory: \"", shortPathName(saga_cmd), "\"", sep=""))
+     }
+      
+      if(is.null(saga_cmd)){
+      warning("Could not locate SAGA GIS! Install program and add it to the Windows registry. See http://www.saga-gis.org/en/ for more info.") 
+      return()
+      }   
+     }
+     else {
+       message(paste("Located SAGA GIS ", saga.version, " from the 'Program Files' directory: \"", shortPathName(saga_cmd), "\"", sep=""))  }
+     }
     
     else {
     
-    if(nzchar(grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ":")[[1]], pattern="FWTools"))) {
+    if(!length(x <- grep(paths <- strsplit(Sys.getenv('PATH')[[1]], ":")[[1]], pattern="FWTools"))==0) {
     fw.dir <- paths[grep(paths, pattern="FWTools")[1]]
     gdalwarp = "gdalwarp"
     gdal_translate = "gdal_translate"
-    message(paste("Located FWTools from the path: \"", fw.dir, "\"", sep=""))
+    message(paste("Located FWTools from the path: \"", shortPathName(fw.dir), "\"", sep=""))
       }
     else { 
-    warning("Install FWTools and add to PATH. See http://fwtools.maptools.org for more info.")
+        warning("Install FWTools and add to PATH. See http://fwtools.maptools.org for more info.")
+        return()
       }
+    
+    if(is.null(im.dir)){ 
+        warning("Install ImageMagick and add to PATH. See http://imagemagick.org for more info.")
+        return()
+        }
+    if(is.null(saga_cmd)){
+        warning("Install SAGA GIS and add to PATH. See http://www.saga-gis.org for more info.")
+        return()
+        }
     }
 
-    lt <- data.frame(gdalwarp, gdal_translate, convert, stringsAsFactors = FALSE)
-    return(lt)
+    lt <- data.frame(gdalwarp, gdal_translate, convert, saga_cmd, stringsAsFactors = FALSE)
+    if(show.paths){  return(lt)  }
 }
 
 ## Standard settings:
@@ -92,7 +140,9 @@ plotKML.env <- function(
     convert,
     gdalwarp,
     gdal_translate,
-    googleAPIkey
+    home_url,
+    googleAPIkey,
+    show.env = TRUE
     ){
     
     require(RColorBrewer)
@@ -102,10 +152,11 @@ plotKML.env <- function(
     if(missing(colour_scale_numeric)) { colour_scale_numeric <- rev(brewer.pal(n = 5, name = "Spectral")) }
     if(missing(colour_scale_factor)) { colour_scale_factor <- brewer.pal(n = 6, name = "Set1") }
     if(missing(xsd)) { xsd <- "http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd" }
-    fw <- paths()
-    if(missing(convert)) { convert <- fw$convert[[1]] }
-    if(missing(gdalwarp)) { gdalwarp <- fw$gdalwarp[[1]] }
-    if(missing(gdal_translate)) { gdal_translate <- fw$gdal_translate[[1]] }
+    pts <- paths(show.paths = TRUE)
+    if(missing(convert)) { convert <- pts$convert[[1]] }
+    if(missing(gdalwarp)) { gdalwarp <- pts$gdalwarp[[1]] }
+    if(missing(gdal_translate)) { gdal_translate <- pts$gdal_translate[[1]] }
+    if(missing(home_url)) { home_url <- "http://plotkml.r-forge.r-project.org/" }
     if(missing(googleAPIkey)) { googleAPIkey <- "ABQIAAAAqHzabFRj8QwDECupLUR4-hT53Nvo2rq6JtI9-sNzq2yJTiKUYBRN5VP8pfrIcMaRo0pNDvBhWJUQCA" }
  
     assign("colour_scale_numeric", colour_scale_numeric, envir=plotKML.opts)
@@ -114,11 +165,13 @@ plotKML.env <- function(
     assign("convert", convert, envir=plotKML.opts)
     assign("gdalwarp", gdalwarp, envir=plotKML.opts)
     assign("gdal_translate", gdal_translate, envir=plotKML.opts)
+    assign("home_url", home_url, envir=plotKML.opts)
     assign("googleAPIkey", googleAPIkey, envir=plotKML.opts)
     
     .plotKML.opts <- list(colour_scale_numeric, colour_scale_factor, xsd, convert, gdalwarp, gdal_translate, googleAPIkey)
     names(.plotKML.opts) <- c("colour_scale_numeric", "colour_scale_factor", "xsd", "convert", "gdalwarp", "gdal_translate", "googleAPIkey")
-    return(.plotKML.opts)
+    
+    if(show.env){  return(.plotKML.opts)  }  
 }
 
 ## Optional: put "library(plotKML); plotKML.env(...)" in your "/etc/Rprofile.site"
