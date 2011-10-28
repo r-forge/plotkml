2fd8226be40da5bb74fc7da5006383a1e8f1a6fe
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-28 09:17:57 +0000

    Color conversion and readGPX functions.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@263 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/col2kml.R
@@ -0,0 +1,50 @@
+# Purpose        : Color conversion functions
+# Maintainer     : Pierre Roudier (pierre.roudier@landcare.nz);
+# Contributions  : Tomislav Hengl (tom.hengl@wur.nl); Dylan Beaudette (debeaudette@ucdavis.edu); 
+# Status         : working version
+# Note           : Converts from HEX to KML and back;
+
+
+## KML colour follow the scheme #aabbggrr
+col2kml <- function(colour){
+
+  # Getting the HEX code
+  res <- rgb(t(col2rgb(colour, alpha = TRUE))/255, alpha = TRUE)
+  # Converting from HEX to KML
+  res <- hex2kml(res)
+
+  res
+}
+
+## Convert hex colours to KML specs
+hex2kml <- function(hex){
+  require(stringr)
+  require(plyr)
+
+  res <- aaply(hex, 1, function(hex){
+    if (str_length(hex) == 9) # if alpha is present
+      res <- paste("#", str_sub(tolower(hex), 8, 9), str_sub(tolower(hex), 6, 7), str_sub(tolower(hex), 4, 5), str_sub(tolower(hex), 2, 3), sep = "")
+    else
+      res <- paste("#ff", str_sub(tolower(hex), 6, 7), str_sub(tolower(hex), 4, 5), str_sub(tolower(hex), 2, 3), sep = "")
+    res
+    }
+  )
+
+  as.vector(res)
+}
+
+## KML spec is alpha-BGR
+kml2hex <- function(kml) {
+  require(stringr)
+  require(plyr)
+
+  res <- aaply(kml, 1, function(x){
+    res <- paste("#", str_sub(toupper(x), 8, 9), str_sub(toupper(x), 6, 7), str_sub(toupper(x), 4, 5), str_sub(toupper(x), 2, 3), sep = "")
+    res
+    }
+  )
+
+  as.vector(res)
+}
+
+# end of script;
