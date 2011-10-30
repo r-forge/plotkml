af41167a3bc27b6518ed95fde041698307d74b85
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-30 11:49:57 +0000

    plotKML.env() HAS to be specified at the beginning of lazy load (now in AAAA.R file).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@278 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/AAAA.R
+++ pkg/R/AAAA.R
@@ -24,11 +24,68 @@ if (!isGeneric("reproject"))
     standardGeneric("reproject")
   )
 
-# setup our environment for storing file handles and the like
+## setup our environment for storing file handles and the like
 plotKML.fileIO <- new.env(hash=TRUE)
 
-# setup the plotKML environment:
+## setup the plotKML environment:
 plotKML.opts <- new.env(hash=TRUE)
 
+## Standard settings:
+plotKML.env <- function(
+    colour_scale_numeric = '',
+    colour_scale_factor = '',
+    ref_CRS,
+    NAflag,
+    kml_xsd,
+    gpx_xsd,
+    convert,
+    gdalwarp,
+    gdal_translate,
+    python,
+    home_url,
+    googleAPIkey,
+    show.env = TRUE,
+    silent = TRUE
+    ){
+    
+    if(missing(colour_scale_numeric)) { colour_scale_numeric <- rev(brewer.pal(n = 5, name = "Spectral")) }
+    if(missing(colour_scale_factor)) { colour_scale_factor <- brewer.pal(n = 6, name = "Set1") }
+    if(missing(ref_CRS)) { ref_CRS <- "+proj=longlat +datum=WGS84" }
+    if(missing(NAflag)) { NAflag <- -99999 }
+    if(missing(kml_xsd)) { kml_xsd <- "http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd" }
+    if(missing(gpx_xsd)) { gpx_xsd <- "http://www.topografix.com/GPX/1/1/gpx.xsd" }
+    
+    if(!silent){
+    pts <- paths(show.paths = TRUE)
+    }
+    else {
+    pts <- data.frame(gdalwarp="", gdal_translate="", convert="", python="", saga_cmd="", stringsAsFactors = FALSE)
+    }
+    
+    if(missing(convert)) { convert <- pts$convert[[1]] }
+    if(missing(gdalwarp)) { gdalwarp <- pts$gdalwarp[[1]] }
+    if(missing(gdal_translate)) { gdal_translate <- pts$gdal_translate[[1]] }
+    if(missing(python)) { python <- pts$python[[1]] }
+    if(missing(home_url)) { home_url <- "http://plotkml.r-forge.r-project.org/" }
+    if(missing(googleAPIkey)) { googleAPIkey <- "ABQIAAAAqHzabFRj8QwDECupLUR4-hT53Nvo2rq6JtI9-sNzq2yJTiKUYBRN5VP8pfrIcMaRo0pNDvBhWJUQCA" }
+ 
+    assign("colour_scale_numeric", colour_scale_numeric, envir=plotKML.opts)
+    assign("colour_scale_factor", colour_scale_factor, envir=plotKML.opts)
+    assign("ref_CRS", ref_CRS, envir=plotKML.opts)
+    assign("NAflag", NAflag, envir=plotKML.opts)
+    assign("kml_xsd", kml_xsd, envir=plotKML.opts)
+    assign("gpx_xsd", gpx_xsd, envir=plotKML.opts)
+    assign("convert", convert, envir=plotKML.opts)
+    assign("gdalwarp", gdalwarp, envir=plotKML.opts)
+    assign("gdal_translate", gdal_translate, envir=plotKML.opts)
+    assign("python", python, envir=plotKML.opts)
+    assign("home_url", home_url, envir=plotKML.opts)
+    assign("googleAPIkey", googleAPIkey, envir=plotKML.opts)
+    
+    plotKML.opts <- list(colour_scale_numeric, colour_scale_factor, ref_CRS, NAflag, kml_xsd, gpx_xsd, convert, gdalwarp, gdal_translate, python, home_url, googleAPIkey)
+    names(plotKML.opts) <- c("colour_scale_numeric", "colour_scale_factor", "ref_CRS", "NAflag", "kml_xsd", "gpx_xsd", "convert", "gdalwarp", "gdal_translate", "python", "home_url", "googleAPIkey")
+    
+    if(show.env){  return(plotKML.opts)  }  
+}
 
 # end of script;
--- pkg/R/ZZZ.R
+++ pkg/R/ZZZ.R
@@ -1,3 +1,8 @@
+# Purpose        : Clean up;
+# Maintainer     : Pierre Roudier (pierre.roudier@landcare.nz);
+# Contributions  : Tomislav Hengl (tom.hengl@wur.nl); Dylan Beaudette (debeaudette@ucdavis.edu); 
+# Status         : tested
+# Note           : for more info see [http://cran.r-project.org/doc/manuals/R-exts.html];
 
 # load plotKML.opts with some basic information
 plotKML.env(show.env = FALSE)
