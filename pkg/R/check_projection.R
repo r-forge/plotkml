b7c4a0296caa2fe3ac6949cfe62343eeb8449e21
thengl (Dr ) <tom.hengl@opengeohub.org>
2011-10-31 11:15:15 +0000

    check_projection.R now works with any arbitrary proj4 string specified via the global settings (plotKML.env(ref_CRS=...))
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@281 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/check_projection.R
+++ pkg/R/check_projection.R
@@ -1,14 +1,13 @@
-#' Extracts the values of PROJ4 parameters
-#'
-#' @param p4s_parameters list of proj4 parameter/value strings
-#' @param param proj4 string parameter to be extracted
-#'
-#' @author Pierre Roudier
+# Purpose        : Extracts PROJ4 parameters and checks if they are compatible with ref_CRS
+# Maintainer     : Pierre Roudier (pierre.roudier@landcare.nz);
+# Contributions  : Tomislav Hengl (tom.hengl@wur.nl); Dylan Beaudette (debeaudette@ucdavis.edu);  
+# Dev Status     : Alpha
+# Note           : p4s_parameters list of proj4 parameter/value strings; Uses the string parsing functionality from the 'plyr' package;
+
 .extractProjValue <- function(p4s_parameters, param){
   
   # Locating the current PROJ4 parameter
   query <- ldply(p4s_parameters, str_locate, pattern = param)
-  if(!is.na(query[1, 1])) {  # if already projection type is missing the string is invalid
   idx <- which(!is.na(query[, 1]) & !is.na(query[, 2]))
   
   # If the PROJ4 parameter is found we extract its value
@@ -16,43 +15,35 @@
     # Selecting the good param string
     param_value <- p4s_parameters[idx]
     # Extract the parameter value
-    value <- str_split(param_value, param)[[1]]
+    value <- strsplit(param_value, param)[[1]]
     value <- value[value != ""]
   }
-  else stop("A valid proj4 string required")
+  else { stop(paste("Proj4string does not contain", param, "parameter. See 'CRS-methods' for more details."))
   }
-  # Otherwise an empty string is returned
-  else
-    value <- ""
 
-  value
+  return(value)
 }
 
 
-#' Parses the PROJ.4 string to check if 
-#' the projection is KML-compatible
-#'
-#' @param p4s the PROJ.4 string of the object to be parsed
-#' @value an array with projection and datum values
-#'
-#' @author Pierre Roudier
-#'
-parse_proj4 <- function(p4s){
+## parse string:
+parse_proj4 <- function(p4s, params){
 
-  # Dependencies
-  require(stringr)
-  require(plyr)
+  if(missing(params)) {
+  ref_CRS = get("ref_CRS", envir = plotKML.opts)
+  value <- strsplit(ref_CRS, "\\+")[[1]]
+  value <- value[value != ""]
+  param_names <- sapply(strsplit(value, "="), function(x){x[1]})
+  params <- as.list(paste("\\+", sapply(strsplit(value, "="), function(x){x[1]}), "=", sep="")) 
+  }
 
-  # Tested PROJ4 params
-  params <- list("\\+proj=", "\\+datum=")
   # Splitting the whole PROJ4 string
   p4s_parameters <- str_split(p4s, " ")[[1]]
   # Extraction of the values of parameters specified above
   res <- laply(params, .extractProjValue, p4s_parameters = p4s_parameters)
   # colnames for better looking result
-  names(res) <- c("proj", "datum")
+  names(res) <- param_names
 
-  res
+  return(res)
 }
 
 getCRS.Spatial <- function(obj) {
@@ -67,35 +58,41 @@ getCRS.Raster <- function(obj) {
 
 setMethod("getCRS", "Raster", getCRS.Raster)
 
-#' Checks that the projection is geographic and the datum is WGS84
-#' for KML creation
-#'
-#' @param obj a sp object
-#' @param logical if TRUE, a logical value is returned. If FALSE, an error is thrown if the test failed.
-#'
-#' @author Pierre Roudier
-#'
-check_projection <- function(obj, logical = TRUE){
+## check proj4string
+check_projection <- function(obj, logical = TRUE, ref_CRS = get("ref_CRS", envir = plotKML.opts)){
   
-  # Dependencies
-  require(rgdal)
-
   # Using PROJ.4 to get the PROJ4 string
   p4s <- getCRS(obj)
 
   # Parsing the PROJ4 string for proj and datum values
   params <- parse_proj4(p4s)
 
+  # the default target proj4 string:
+  value <- strsplit(ref_CRS, "\\+")[[1]]
+  value <- value[value != ""]
+  target_params <- stringr::str_trim(sapply(strsplit(value, "="), function(x){x[2]}))
+  names(target_params) <- sapply(strsplit(value, "="), function(x){x[1]})
+
+  # if already projection type is missing the string is invalid
+  if(params["proj"] != ""){  
+
   # If test fails
-  if (params["proj"] != "longlat" | params["datum"] != "WGS84") {
+  if (sum(is.na(match(params, target_params)))>0) {
     if (!logical)
-      stop("Geographical coordinates required.")
+      stop(paste("'", ref_CRS, "' coordinate system required."))
     else
       res <- FALSE
   }
   # If test succeed
   else
     res <- TRUE
+  }
+  
+  else {
+    stop("A valid proj4string required. See 'CRS-methods' for more details.")
+    }
 
-  res
+  return(res)
 }
+
+# end of script;
