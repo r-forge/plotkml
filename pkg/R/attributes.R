4ae01cdeb30fca885a31c7b37dc8410864280e8b
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-09 22:01:03 +0000

    Updated the attributes.R script (dataframe to html description table).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@306 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/attributes.R
+++ pkg/R/attributes.R
@@ -4,40 +4,24 @@
 # Status         : ready for R-forge
 # Note           : required by KML writing functions;
 
-.df_to_kml_html_table <- function(x, columns=TRUE) {
-  
-  # get our invisible file connection from custom evnrionment
-  file.connection <- get('kml.file.out', env=plotKML.fileIO)
-  
-  
-  # if the user passed in TRUE, then we want all of the columns
-  if(class(columns) == 'logical')
-      columns <- 1:ncol(x) # use all columns from the dataframe row
-  
-  # otherwise, keep only requested columns
-  x <- x[, columns]
-  x.names <- names(x)
-  
-  # empty list to store html elements
-  html.list <- list()
-  
-  # create vector of attribute column names, padded with HTML styling
-  att.names <- sapply(x.names, function(i) { paste('<span style="font-weight: bold; color: #000000; padding:3px;">', as.character(i), '</span>:&nbsp;', sep = '') })
- 
-  # create vector of styled attribute values
-  att.values <- sapply(x, function(i) { paste('<span style="color: #000000; padding:3px;">', as.character(i), '</span><br>', sep = '') })
-  
-  # combine by interleaving:
-  att <- paste(att.names, att.values, collapse="\n")
-  
-  # format the output: consider adding a heuristic for adjusting ballon div width
-  html.list <- append(html.list, '<description><![CDATA[')
-  html.list <- append(html.list, '<div line-height:1.25; style="width:600px; border:1px solid; padding:3px ">')
-  html.list <- append(html.list, att)
-  html.list <- append(html.list, '</div>')
-  html.list <- append(html.list, ']]></description>\n')
+.df2htmltable <- function(x, columns=TRUE) {
 
-  cat(paste(html.list, collapse = "\n"), file = file.connection, append = TRUE)
+    # if the user passed in TRUE, then we want all of the columns
+    if(class(columns) == 'logical'){
+        columns <- 1:ncol(x) # use all columns from the dataframe row
+    }
+    else{     # otherwise, keep only requested columns
+    x <- x[, columns]
+    }
+    
+    # get selected table data:
+    att.names <- sapply(names(x), function(i) { paste('<span style="font-weight: bold; color: #000000; padding: 3px;">', as.character(i), '</span>:&nbsp;', sep = '') } )    
+    att.values <- as.vector(t(sapply(x, function(i) { paste('<span style="color: #000000; padding:3px;">', as.character(i), '</span><br>', sep = '') })))
+    # combine by interleaving:
+    att <- matrix(paste(att.names, att.values, sep="\n"), ncol=length(names(x)), byrow=TRUE)
+    html.table <- apply(att, 1, paste, collapse="\n")
+    
+    return(html.table) 
 }
 
 # end of script;
