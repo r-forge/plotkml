064ef0f1c899f95d6d182dc0a9735d79f58381de
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-25 00:49:19 +0000

    fixed missing libraries in the examples.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@436 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plotKML.SpatialPredictions.R
+++ pkg/R/plotKML.SpatialPredictions.R
@@ -11,7 +11,7 @@ setMethod("plotKML", "SpatialPredictions", function(
   file.name = paste(folder.name, ".kml", sep=""),
   colour_scale_svar = get("colour_scale_svar", envir = plotKML.opts),
   grid2poly = FALSE,
-  obj.summary = FALSE,
+  obj.summary = TRUE,
   plot.svar = FALSE,
   pngwidth = 210, 
   pngheight = 580,
@@ -34,7 +34,7 @@ setMethod("plotKML", "SpatialPredictions", function(
     
   # summary properties of the RK model:
   if(obj.summary==TRUE){
-    xx <- GSIF::summary(obj)
+    xx <- summary(obj)
     xd <- unlist(xx[!names(xx) %in% c("bonds", "breaks")])
     md <- data.frame(Names=attr(xd, "names"), Values=xd,  stringsAsFactors = FALSE)
     html <- kml_description(md, asText = TRUE, cwidth = 120, twidth = 240)
@@ -68,26 +68,28 @@ setMethod("plotKML", "SpatialPredictions", function(
   kml_layer(obj = locs, points_names = labs)  
 
   # plot the correlation graph and variogram:
-  png(filename=paste(varname, "_gstatplots.png", sep=""), width=pngwidth, height=pngheight, bg="white", pointsize=pngpointsize)
-  par(mfrow=c(3,1))
-  par(mar=c(4.5,4.5,.8,.8))
-  plot(y=obj@validation$var1.pred, x=obj@validation$observed, pch=19, asp=1, col = "red", xlab='observed', col.main = rgb(0.99,0.99,0.99), ylab='predicted')
-  vv <- variogram(as.formula("observed~1"), obj@validation)
-  plot(x=vv$dist, y=vv$gamma, pch="+", col = "green", xlab='distance', cex=1.4, ylab='gamma', ylim = c(0, max(vv$gamma)))
-  lines(x=c(0, vv$dist), y=rep(var(obj@validation$observed), length(vv$dist)+1), lty=2)
-  # Residuals:
-  vgmmodel = obj@vgmModel
-  class(vgmmodel) <- c("variogramModel", "data.frame")
-  vvres <- variogram(as.formula(paste(paste(varname, "residual", sep="."), "~ 1")), obj@observed[paste(varname, "residual", sep=".")]) # model residuals
-  vve <- variogram(residual ~ 1, obj@validation["residual"]) # validation residuals
-  plot(x=vvres$dist, y=vvres$gamma, pch="+", col = "green", xlab='distance', cex=1.4, ylab='gamma', ylim = c(0, max(vv$gamma)))
-  points(x=vve$dist, y=vve$gamma, pch="+", col = "red", cex=1.4)
-  vline <- variogramLine(vgmmodel, maxdist=max(vvres$dist), n=length(vvres$dist))
-  lines(x=vline$dist, y=vline$gamma, col = "green", lwd=2)
-  dev.off()
+  if(all(!is.na(obj@validation$var1.pred)) & all(!is.na(obj@validation$observed))){
+    png(filename=paste(varname, "_gstatplots.png", sep=""), width=pngwidth, height=pngheight, bg="white", pointsize=pngpointsize)
+    par(mfrow=c(3,1))
+    par(mar=c(4.5,4.5,.8,.8))
+    plot(y=obj@validation$var1.pred, x=obj@validation$observed, pch=19, asp=1, col = "red", xlab='observed', col.main = rgb(0.99,0.99,0.99), ylab='predicted')
+    vv <- variogram(as.formula("observed~1"), obj@validation)
+    plot(x=vv$dist, y=vv$gamma, pch="+", col = "green", xlab='distance', cex=1.4, ylab='gamma', ylim = c(0, max(vv$gamma)))
+    lines(x=c(0, vv$dist), y=rep(var(obj@validation$observed), length(vv$dist)+1), lty=2)
+    # Residuals:
+    vgmmodel = obj@vgmModel
+    class(vgmmodel) <- c("variogramModel", "data.frame")
+    vvres <- variogram(as.formula(paste(paste(varname, "residual", sep="."), "~ 1")), obj@observed[paste(varname, "residual", sep=".")]) # model residuals
+    vve <- variogram(residual ~ 1, obj@validation["residual"]) # validation residuals
+    plot(x=vvres$dist, y=vvres$gamma, pch="+", col = "green", xlab='distance', cex=1.4, ylab='gamma', ylim = c(0, max(vv$gamma)))
+    points(x=vve$dist, y=vve$gamma, pch="+", col = "red", cex=1.4)
+    vline <- variogramLine(vgmmodel, maxdist=max(vvres$dist), n=length(vvres$dist))
+    lines(x=vline$dist, y=vline$gamma, col = "green", lwd=2)
+    dev.off()
   
   # add the variogram plot:
   kml_screen(image.file = paste(varname, "_gstatplots.png", sep=""), position = "LL", sname = "gstatModel summary plot")
+  }
 
   # close the file:
   kml_close(file.name = file.name)
