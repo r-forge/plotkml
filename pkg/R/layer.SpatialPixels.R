f3a5e159b4bb304c444bbde2eed04d9ce5a7bda5
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-18 19:24:50 +0000

    removed "..." from the kml / open
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@451 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/layer.SpatialPixels.R
+++ pkg/R/layer.SpatialPixels.R
@@ -92,7 +92,7 @@ kml_layer.SpatialPixels <- function(
   }
 
   # Plotting the image
-  png(filename = raster_name, bg = "transparent", type="cairo-png", width=png.width, height=png.height)
+  png(filename = raster_name, bg = "transparent", type = "cairo-png", width = png.width, height = png.height)
   par(mar = c(0, 0, 0, 0), xaxs = "i", yaxs = "i")
   if(!is.na(charmatch("z.lim", names(call)))){ 
     z.lim <- eval(call[["z.lim"]]) 
