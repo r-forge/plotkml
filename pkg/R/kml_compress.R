abedced8f95d60cf15bcaf257b5de8650514bdb5
thengl (Dr ) <tom.hengl@opengeohub.org>
2019-11-12 02:17:34 +0000

    added SPC
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotkml@576 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/kml_compress.R
+++ pkg/R/kml_compress.R
@@ -6,7 +6,11 @@ kml_compress <- function(file.name, zip = Sys.getenv("R_ZIPCMD", "zip"), files =
   kmz <- stringr::str_replace(file.name, extension, "kmz") # switch the extension to kmz
 	
   # use R's zip wrapper
-  try( x <- zip(zipfile = utils::shortPathName(kmz), files= utils::shortPathName(file.name), zip = zip) )
+  if(.Platform$OS.type == "windows") {
+    try( x <- zip(zipfile = utils::shortPathName(kmz), files= utils::shortPathName(file.name), zip = zip) )
+  } else {
+    try( x <- zip(zipfile = kmz, files= file.name, zip = zip) )
+  }
   # Error handling
   if(is(.Last.value, "try-error")| x==127) {
     if(zip==""|!nzchar(zip)){
